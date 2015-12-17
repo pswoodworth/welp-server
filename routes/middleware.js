141c0c8be4ea9082578c1bcdf9dbb16dcf5f51bf
pswoodworth <pswoodworth@gmail.com>
2015-12-17 01:29:51 -0500

    removed a bunch of default keystone garbage

--- routes/middleware.js
+++ routes/middleware.js
@@ -1,8 +1,8 @@
 /**
  * This file contains the common middleware used by your routes.
- * 
+ *
  * Extend or replace these functions as your application requires.
- * 
+ *
  * This structure is not enforced, and just a starting point. If
  * you have more middleware you may want to group it as separate
  * modules in your project's /lib directory.
@@ -13,24 +13,35 @@ var _ = require('underscore');
 
 /**
 	Initialises the standard view locals
-	
+
 	The included layout depends on the navLinks array to generate
 	the navigation in the header, you may wish to change this array
 	or replace it with your own templates / logic.
 */
 
 exports.initLocals = function(req, res, next) {
-	
+
 	var locals = res.locals;
-	
+
 	locals.navLinks = [
 		{ label: 'Home',		key: 'home',		href: '/' }
 	];
-	
+
 	locals.user = req.user;
-	
+
 	next();
-	
+
+};
+
+exports.ensureAPIKey = function(req, res, next) {
+
+	var apiKey = process.env.API_KEY;
+	if (req.query.key !== apiKey){
+		res.status(401).send('¯\\_(ツ)_/¯ not today');
+	}else{
+		next();
+	}
+
 };
 
 
@@ -39,18 +50,18 @@ exports.initLocals = function(req, res, next) {
 */
 
 exports.flashMessages = function(req, res, next) {
-	
+
 	var flashMessages = {
 		info: req.flash('info'),
 		success: req.flash('success'),
 		warning: req.flash('warning'),
 		error: req.flash('error')
 	};
-	
+
 	res.locals.messages = _.any(flashMessages, function(msgs) { return msgs.length; }) ? flashMessages : false;
-	
+
 	next();
-	
+
 };
 
 
@@ -59,12 +70,12 @@ exports.flashMessages = function(req, res, next) {
  */
 
 exports.requireUser = function(req, res, next) {
-	
+
 	if (!req.user) {
 		req.flash('error', 'Please sign in to access this page.');
 		res.redirect('/keystone/signin');
 	} else {
 		next();
 	}
-	
+
 };
